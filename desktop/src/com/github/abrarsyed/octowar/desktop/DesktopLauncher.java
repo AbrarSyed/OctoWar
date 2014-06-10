7628296c8e1af79833ebf60a42718ada2120dbac
ashes999 <ashes999@yahoo.com>
2014-06-10 18:29:13 -0400

    Reinitialized repo with latest libGDX template

---
+++ desktop/src/com/github/abrarsyed/octowar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.abrarsyed.octowar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.abrarsyed.octowar.OctoWarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OctoWarGame(), config);
+	}
+}
