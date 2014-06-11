c47e5430fda9f73ddf978bb9e6593fa61537c749
ashes999 <ashes999@yahoo.com>
2014-06-11 17:51:54 -0400

    Added placeholder BlastCube project. With gradle too.

---
+++ blastcube/src/com/blastcube/component/SpriteComponent.java
@@ -0,0 +1,5 @@
+package com.blastcube.component;
+
+public class SpriteComponent {
+
+}
--- core/src/com/github/abrarsyed/octowar/OctoWarGame.java
+++ core/src/com/github/abrarsyed/octowar/OctoWarGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.blastcube.component.SpriteComponent;
 
 public class OctoWarGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -14,6 +15,7 @@ public class OctoWarGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+        new SpriteComponent();
 	}
 
 	@Override
