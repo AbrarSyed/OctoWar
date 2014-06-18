5649272ef5b9267a1c5c778661d41f2d01401dfd
ashes999 <ashes999@yahoo.com>
2014-06-18 18:02:59 -0400

    Player moves on core game screen

--- core/src/com/github/abrarsyed/octowar/scenes/CoreGameScene.java
+++ core/src/com/github/abrarsyed/octowar/scenes/CoreGameScene.java
@@ -1,13 +1,33 @@
 package com.github.abrarsyed.octowar.scenes;
 
 import com.blastcube.Scene;
+import com.blastcube.component.KeyboardInputComponent;
 import com.blastcube.component.SpriteComponent;
+import com.blastcube.component.internal.Action;
 import com.blastcube.entity.Entity;
 
 public class CoreGameScene extends Scene {
 
 	public CoreGameScene() {
-		this.addEntity(new Entity(new SpriteComponent("images/player.png")));
+		final int MOVEMENT_INCREMENT = 32;
+		
+		final Entity me = new Entity(new SpriteComponent("images/player.png"));
+		me.add(new KeyboardInputComponent(new Action<String>() {
+			@Override
+			public void call(String key) {
+				SpriteComponent s = me.get(SpriteComponent.class);
+				if (key == "left") {
+					s.setX(s.getX() - MOVEMENT_INCREMENT);
+				} else if (key == "right") {
+					s.setX(s.getX() + MOVEMENT_INCREMENT);
+				} else if (key == "up") {
+					s.setY(s.getY() - MOVEMENT_INCREMENT);
+				} else if (key == "down") {
+					s.setY(s.getY() + MOVEMENT_INCREMENT);
+				}
+			}
+		}));
+		this.addEntity(me);
 	}
 	
 }
